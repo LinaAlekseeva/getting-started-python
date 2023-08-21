40bbe1bbe572fcc5a311acb624b8dc56cb108d4e
Lina Alekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-08-21 09:40:38 +0300

    Update test_google_should_find_selene.py

--- getting-started-python-master/tests/test_google_should_find_selene.py
+++ getting-started-python-master/tests/test_google_should_find_selene.py
@@ -5,5 +5,5 @@ from selene import be, have
 def test_open():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
     test_open()
