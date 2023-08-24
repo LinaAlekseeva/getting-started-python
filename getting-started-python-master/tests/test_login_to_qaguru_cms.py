cbc84a911f7a6181cce8c08a40820bc0b2ee9ebf
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-08-24 11:45:03 +0300

    + add changes

--- getting-started-python-master/tests/test_login_to_qaguru_cms.py
+++ getting-started-python-master/tests/test_login_to_qaguru_cms.py
@@ -6,6 +6,6 @@ def test_valid_login():
     browser.open('https://demoqa.com/text-box')
     browser.element('#userName').type('Lina Alekseeva').press_tab()
     browser.element('#userEmail').type('qa@guru.ru').press_tab()
-    browser.element('.col-md-9.col-sm-12').type('Address').press_tab()
+    browser.element('#currentAddress').type('Address').press_tab()
     browser.element('#permanentAddress').type('Permanent Address').press_enter()
     browser.element('#submit').click()
